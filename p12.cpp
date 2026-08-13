c508a1cf6e872a7f242096d6ef5f0ae3933fe368
saksham007007 <sakshampokhriyal2006@gmail.com>
2026-08-13 08:49:43 +0530

    Add files via upload

---
+++ p12.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class Time{
+int hr;
+int min;
+int sec;
+public:
+
+Time(){
+hr =0,min=0,sec=0;
+}
+
+Time(int hr,int min, int sec){
+this->hr =hr;
+this->min=min;
+this->sec=sec;
+}
+
+void show(){
+cout<<hr<<":"<<min<<":"<<sec<<endl;
+}
+
+void add(Time &t1,Time &t2){
+hr = t1.hr + t2.hr;
+min = t1.min + t2.min;
+if(min>60){
+hr = hr +1;
+min = min -60;
+}
+sec = t1.sec + t2.sec;
+if(sec>60){
+min = min+1;
+sec = sec -60;
+}
+}
+};
+
+int main(){
+Time t1(12,10,20);
+Time t2(50,59,59);
+Time t3;
+t3.add(t1,t2);
+t1.show();
+t2.show();
+t3.show();
+return 0;
+}
