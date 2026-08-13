51407d12aa99224047893a14c9e338d58ab27fec
saksham007007 <sakshampokhriyal2006@gmail.com>
2026-08-13 08:55:11 +0530

    Add files via upload

--- p12.cpp
+++ p12.cpp
@@ -25,24 +25,22 @@ void add(Time &t1,Time &t2){
 hr = t1.hr + t2.hr;
 min = t1.min + t2.min;
 if(min>60){
-hr = hr +1;
-min = min -60;
+hr = hr + min/60;
+min = min %60;
 }
 sec = t1.sec + t2.sec;
 if(sec>60){
-min = min+1;
-sec = sec -60;
+min = min+sec/60;
+sec = sec%60;
 }
 }
 };
 
 int main(){
-Time t1(12,10,20);
-Time t2(50,59,59);
+Time t1(12,140,201);
+Time t2(50,130,592);
 Time t3;
 t3.add(t1,t2);
-t1.show();
-t2.show();
 t3.show();
 return 0;
 }
